3aae52ea2fe89f7556d348e5f288a0eb36a9e9ef
rri <raul.rincones@adorsys.com>
2023-11-01 17:32:25 +0100

    Chat websocket io almost ready

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('Clients Connected: ', io.engine.clientsCount);
+  console.log('Socket connected ID ', socket.id);
+
+  //Listen to the client disconnected
+  socket.on('disconnect', () => {
+    console.log('The socket ' + socket.id + ' is disconnected');
+  });
+
+  socket.connectedRoom = '';
+
+  socket.on('connect to room', (room) => {
+    socket.leave(socket.connectedRoom);
+
+    switch (room) {
+      case 'room1':
+        socket.join('room1');
+        socket.connectedRoom = 'room1';
+        break;
+
+      case 'room2':
+        socket.join('room2');
+        socket.connectedRoom = 'room2';
+        break;
+
+      case 'room3':
+        socket.join('room3');
+        socket.connectedRoom = 'room3';
+        break;
+    }
+  });
+
+  socket.on('message', (message) => {
+    const room = socket.connectedRoom;
+
+    io.to(room).emit('send message', {
+      message,
+      room,
+    });
+  });
+});
+
+httpServer.listen(3000);
